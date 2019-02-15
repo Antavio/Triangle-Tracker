55e26f6f01f59156904b38b24856ce6cb33eabb0
Anthony Njuguna <njuguna13@gmail.com>
2019-02-15 14:18:47 +0300

    Converted user input strings to numbers.

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function tracker(){
+  var lengthOne = parseInt(document.getElementById("Length-one"));
+  var lengthTwo = parseInt(document.getElementById("Length-two"));
+  var lengthThree = parseInt(document.getElementById("Length-three"));
+  
+}
