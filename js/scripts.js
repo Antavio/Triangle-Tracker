6f0e28cf7553d04b784419fc600b62b30f6d97d7
Anthony Njuguna <njuguna13@gmail.com>
2019-02-15 16:25:51 +0300

    Added arrays to store input values.

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,20 @@ function tracker(){
   var lengthTwo = parseInt(document.getElementById("Length-two").value);
   var lengthThree = parseInt(document.getElementById("Length-three").value);
 
-  var storedArray = [lengthOne,lengthTwo,lengthThree];
-  if(storedArray[0] > 0 && storedArray[1] > 0 && storedArray[2]){
-    
+  var storedArray = [lengthOne, lengthTwo, lengthThree];
+  if (storedArray[0] > 0 && storedArray[1] > 0 && storedArray[2]  > 0){
+  	if (storedArray[0]  + storedArray[1] <= storedArray[2] || storedArray[0] + storedArray[2] <= storedArray[1] || storedArray[1] + storedArray[2] < storedArray[0]){
+    alert("Cannot form a triangle")
+    } else {
+    	if(storedArray[0] == storedArray[1] && storedArray[1] == storedArray[2]){
+      	alert("Equilateral Triangle");
+      } else if ((storedArray[0] == storedArray[1]) || (storedArray[1] == storedArray[2]) || (storedArray[0] == storedArray[2])){
+      alert("Isosceles Triangle");
+    } else {
+      	alert("Scalene");
+      }
+    }
   } else {
-    alert("Enter values above zero");
+  	alert ("Entered values should be greater than zero");
   }
+}
