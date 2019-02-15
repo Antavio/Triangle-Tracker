2bacc47b9d75a0b42d7de4f02a9259fd304126f3
Anthony Njuguna <njuguna13@gmail.com>
2019-02-15 15:41:09 +0300

    Validated inputs to be greater than zero.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,10 @@ function tracker(){
   var lengthOne = parseInt(document.getElementById("Length-one").value);
   var lengthTwo = parseInt(document.getElementById("Length-two").value);
   var lengthThree = parseInt(document.getElementById("Length-three").value);
-  if (lengthOne == lengthTwo && lengthTwo == lengthThree){
-	alert("Equilateral");
-} else if ((lengthOne == lengthTwo) || (lengthTwo == lengthThree) || (lengthOne == lengthThree)){
-alert("isosceles");
-} else if((lengthOne != lengthTwo !=lengthThree) && ((lengthOne + lengthTwo > lengthThree) && (lengthOne + lengthThree > lengthTwo) && (lengthTwo + lengthThree > lengthOne))) {
-	alert("Scalene");
-} else {
-	alert("Cannot make a Triangle");
-}
-}
+
+  var storedArray = [lengthOne,lengthTwo,lengthThree];
+  if(storedArray[0] > 0 && storedArray[1] > 0 && storedArray[2]){
+    
+  } else {
+    alert("Enter values above zero");
+  }
