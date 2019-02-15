766a62c670a50f2c0d7d8eb4616325e44c45753d
Anthony Njuguna <njuguna13@gmail.com>
2019-02-15 14:25:47 +0300

    Added if Condition to determine type of triangle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 function tracker(){
-  var lengthOne = parseInt(document.getElementById("Length-one"));
-  var lengthTwo = parseInt(document.getElementById("Length-two"));
-  var lengthThree = parseInt(document.getElementById("Length-three"));
-  
+  var lengthOne = parseInt(document.getElementById("Length-one").value);
+  var lengthTwo = parseInt(document.getElementById("Length-two").value);
+  var lengthThree = parseInt(document.getElementById("Length-three").value);
+  if (lengthOne == lengthTwo && lengthTwo == lengthThree){
+	alert("Equilateral");
+} else if ((lengthOne == lengthTwo) || (lengthTwo == lengthThree) || (lengthOne == lengthThree)){
+alert("isosceles");
+} else if((lengthOne != lengthTwo !=lengthThree) && ((lengthOne + lengthTwo > lengthThree) && (lengthOne + lengthThree > lengthTwo) && (lengthTwo + lengthThree > lengthOne))) {
+	alert("Scalene");
+} else {
+	alert("Cannot make a Triangle");
+}
 }
